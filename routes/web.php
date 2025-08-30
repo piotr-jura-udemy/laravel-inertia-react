c079fa67d3c75aee1b7b01dd85d3b719c21ff1ac
Piotr Jura <jura.piotrek@gmail.com>
2025-08-30 19:36:12 +0200

    008-setting-up-inertiajs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Request -> Laravel -> http://localhost:8000 -> [Middleware 1] -> Route -> Response
+
 Route::get('/', function () {
     return view('welcome');
 });
