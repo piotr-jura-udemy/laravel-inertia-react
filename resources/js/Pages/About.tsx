ab3757727a0144582960a9e94a6b7473ad1a9ca0
Piotr Jura <jura.piotrek@gmail.com>
2025-08-31 13:07:21 +0200

    010-link

--- resources/js/Pages/About.tsx
+++ resources/js/Pages/About.tsx
@@ -1,8 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 export default function About() {
     return (
         <div>
             <h1>About</h1>
             <div>Welcome to about page</div>
+            <Link href="/">Home</Link>
         </div>
     );
 }
--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,8 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
             <div>Welcome to homepage</div>
+            <Link href="/about">About</Link>
         </div>
     );
 }
