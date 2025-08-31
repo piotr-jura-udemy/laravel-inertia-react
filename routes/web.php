ca2e21b23153b4e043af54bbd5431cf56871f5cd
Piotr Jura <jura.piotrek@gmail.com>
2025-08-31 11:33:55 +0200

    009-creating-pages

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 // Request -> Laravel -> http://localhost:8000 -> [Middleware 1] -> Route -> Response
 
+// http://localhost:8000
+
+// http://localhost:8000 + /
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
+});
+
+// http://localhost:8000/about
+Route::get('/about', function () {
+    return Inertia::render('About');
 });
