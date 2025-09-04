188937dc50a8eac25332e78f1547105c804a9bdc
Piotr Jura <jura.piotrek@gmail.com>
2025-09-04 20:12:35 +0200

    022-one-to-many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
