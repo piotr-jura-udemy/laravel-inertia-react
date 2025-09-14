5da6bc963e4146795db0c1492be437e7775b53d3
Piotr Jura <jura.piotrek@gmail.com>
2025-09-14 19:14:24 +0200

    034-comment-relations-and-migration

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    protected $fillable = ['body', 'post_id', 'user_id'];
+
+    public function post(): BelongsTo {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
