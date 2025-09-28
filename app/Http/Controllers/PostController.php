08253bae200d92cb145e2e55470ef37314b46f96
Piotr Jura <jura.piotrek@gmail.com>
2025-09-28 17:40:43 +0200

    056-relationship-existance

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
@@ -11,7 +12,8 @@ use Inertia\Response;
 
 class PostController extends Controller
 {
-    public function index(): Response {
+    public function index(): Response
+    {
         // likes_count
         return Inertia::render('posts/index', [
             'posts' => Post::with('user')->withCount('likes')->latest()->get()
@@ -19,7 +21,8 @@ class PostController extends Controller
     }
 
     // index, show, edit, update...
-    public function show(string $id): Response {
+    public function show(string $id): Response
+    {
         $post = Post::with('user')->findOrFail($id);
         return Inertia::render('posts/show', [
             'post' => $post,
@@ -28,20 +31,31 @@ class PostController extends Controller
                     ->with('user')
                     ->latest()
                     ->get()
+            ),
+            'likes' => Inertia::defer(
+                fn() => [
+                    'count' => $post->likes()->count(),
+                    'user_has_liked' => $post->likes()->where([
+                        'ip_address' => request()->ip(),
+                        'user_agent' => request()->userAgent()
+                    ])->exists()
+                ]
             )
         ]);
     }
 
-    public function create(): Response {
+    public function create(): Response
+    {
         return Inertia::render('posts/create');
     }
 
-    public function store(Request $request): RedirectResponse {
+    public function store(Request $request): RedirectResponse
+    {
         $validated = $request->validate([
             'title' => 'required|string|min:3|max:255',
             'body' => 'required|string|min:10|max:255'
         ]);
-        
+
         Post::create([
             ...$validated,
             'user_id' => User::inRandomOrder()->first()->id
