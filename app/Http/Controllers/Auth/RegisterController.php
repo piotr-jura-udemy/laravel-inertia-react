441cc6937862c01e11026e9b5efbd16e297f8f22
Piotr Jura <jura.piotrek@gmail.com>
2025-10-02 18:11:52 +0200

    062-auth-forms

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisterController extends Controller
+{
+    public function create(): Response
+    {
+        return Inertia::render('auth/register/create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostToggleLike;
@@ -21,6 +23,13 @@ Route::get('/about', function () {
 })->name('about.index');
 
 // http://localhost:8000/1
+
+Route::get('/auth/register', [RegisterController::class, 'create']);
+Route::post('/auth/register', [RegisterController::class, 'store']);
+
+Route::get('/auth/login', [LoginController::class, 'create']);
+Route::post('/auth/login', [LoginController::class, 'store']);
+Route::post('/auth/logout', [LoginController::class, 'destroy']);
 
 Route::post('/posts/{post}/likes/toggle', PostToggleLike::class);
 
