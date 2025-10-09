efb82f8c986eb69b03ce563f906073e8a8e5cfe4
Piotr Jura <jura.piotrek@gmail.com>
2025-10-09 19:59:12 +0200

    065-shared-data-user

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             //
+            'user' => $request->user(),
         ];
     }
 }
